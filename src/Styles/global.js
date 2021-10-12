07e7b5ab30d2f8c8a65784c12976512fbd7ecabf
Anthony Freitas <anthonyfreitas2015@gmail.com>
2021-10-12 15:40:14 -0400

    first commit

---
+++ src/Styles/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        --Main-Orange-Primary: #F27D3C;
+        --Main-Orange-Secondary: #E18450;
+        --Main-Black-Primary: #544848;
+        --Main-White: #ffffff;
+        --Main-Grey-Primary: #6B6868;
+        --Main-Grey-Secondary: #7E7D7D;
+    }
+    
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+`
