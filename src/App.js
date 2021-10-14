0d1e617dc6e851c9d9ee1b09e141031479b8de78
Anthony Freitas <anthonyfreitas2015@gmail.com>
2021-10-13 21:55:50 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { GlobalStyle } from "./Styles/global"
-import { Routes } from "./Routes"
-import { Header } from "./Components/Header";
+
 export const App = () => {
   return (
     <div className="App">
       <GlobalStyle/>
-      <Header Tile="Delivery"/>
-      <Routes/>
     </div>
   );
 }
